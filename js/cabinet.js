67397d7825ba6fe49851a4e7093888f2be8088ba
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-23 22:51:30 +0400

    added changes

--- js/cabinet.js
+++ js/cabinet.js
@@ -1,12 +1,26 @@
-const element = document.querySelectorAll('.element4');
-element.forEach(function(el, key){                
-    el.addEventListener('click', function () {
-        el.classList.add("active4");
-            
-        element.forEach(function(ell, index){
-            if(key !== index) {
-                ell.classList.remove('active4');
+var toggleDiv = function(self, id) {
+    var li = self.parentNode,
+        contents = document.getElementsByClassName('content'),
+        menu = document.getElementById('info-menu'),
+        children = menu.children,
+        child = undefined,
+        i = 0;
+
+    // Loop over all content divs and show the active and hide all others.
+    for (i = 0; i < contents.length; i++)
+        contents[i].style.display = contents[i].id == id ? 'block' : 'none';
+
+    // Loop over all menu items and add active class to the selected
+    // and remove from others if applicable.
+    for (i = 0; i < children.length; i++) {
+        child = children[i];
+
+        if (child === li) {
+            child.className = (child.className + ' active').trim();
+        } else {
+            if (child.className.indexOf('active') > -1) {
+                child.className = child.className.replace('active', '').trim();
             }
-        });
-    });
-});
+        }
+    }
+}
