f90c28853c74df1bdbfba927786ee1745cb43376
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-28 16:08:51 +0400

    update

--- js/cabinet.js
+++ js/cabinet.js
@@ -1,26 +1,12 @@
-var toggleDiv = function(self, id) {
-    var li = self.parentNode,
-        contents = document.getElementsByClassName('content'),
-        menu = document.getElementById('info-menu'),
-        children = menu.children,
-        child = undefined,
-        i = 0;
-
-    // Loop over all content divs and show the active and hide all others.
-    for (i = 0; i < contents.length; i++)
-        contents[i].style.display = contents[i].id == id ? 'block' : 'none';
-
-    // Loop over all menu items and add active class to the selected
-    // and remove from others if applicable.
-    for (i = 0; i < children.length; i++) {
-        child = children[i];
-
-        if (child === li) {
-            child.className = (child.className + ' active').trim();
-        } else {
-            if (child.className.indexOf('active') > -1) {
-                child.className = child.className.replace('active', '').trim();
+const element = document.querySelectorAll('.prof-element');
+element.forEach(function(el, key){                
+    el.addEventListener('click', function () {
+        el.classList.add("prof-active");
+            
+        element.forEach(function(ell, index){
+            if(key !== index) {
+                ell.classList.remove('prof-active');
             }
-        }
-    }
-}
+        });
+    });
+});
