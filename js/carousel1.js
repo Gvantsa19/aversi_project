e5b4969052616a07ad62ead7a15f0dc6c11d2eb5
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-28 22:30:35 +0400

    update

---
+++ js/carousel1.js
@@ -0,0 +1,17 @@
+// let slidePosition = 0;
+// const slides = document.getElementsByClassName("carousel__item");
+// ​
+// const nextButton = document.querySelector('#carousel__btn--next');
+// const prevButton = document.querySelector('#carousel__btn--prev')
+// ​
+// const carouselDiv = document.querySelector('.animation');
+// ​
+// nextButton.addEventListener('click', () => {
+//     carouselDiv.classList.replace('carousel_animation_left','carousel_animation_right');
+//     carouselDiv.style.transform = "translateX(-100%)";
+// })
+// prevButton.addEventListener('click', () => {
+//     carouselDiv.classList.replace('carousel_animation_right','carousel_animation_left');
+//     carouselDiv.style.transform = "translateX(0)";
+// })
+// ​
