1ffea16bb880938f496d35a3d6ac9f79fec2cdf2
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-09-13 18:12:33 +0400

    update

--- js/preview-photos.js
+++ js/preview-photos.js
@@ -5,7 +5,7 @@ const productPreview = document.querySelector('.product_preview');
         const rightSide = document.createElement('div');
         productPreview.appendChild(rightSide);
         rightSide.classList.add('right-side');
-        rightSide.style.cssText+= `background-image: url('./images/back.png'); margin-top: 50px;`;
+        rightSide.style.cssText+= `background-image: url('./images/back.png'); margin-top: 50px; background-size: cover;`;
 
         images.forEach(image => {
             image.addEventListener('click', function() {
--- js/script.js
+++ js/script.js
@@ -40,65 +40,65 @@ $("#burger-menu").on("click", function(){
       updateSlidesPosition();
   }
 
-// slider2 + hide and show
-const element = document.querySelectorAll('.element');
-element.forEach(function(el, key){                
-    el.addEventListener('click', function () {
-        el.classList.add("active");
+// // slider2 + hide and show
+// const element = document.querySelectorAll('.element');
+// element.forEach(function(el, key){                
+//     el.addEventListener('click', function () {
+//         el.classList.add("active");
             
-        element.forEach(function(ell, index){
-            if(key !== index) {
-                ell.classList.remove('active');
-            }
-        });
-    });
-});
+//         element.forEach(function(ell, index){
+//             if(key !== index) {
+//                 ell.classList.remove('active');
+//             }
+//         });
+//     });
+// });
 
-let slide2 = 0;
-const slides2 = document.getElementsByClassName("carousel__item2");
-const totalslides2 = slides2.length;
+// let slide2 = 0;
+// const slides2 = document.getElementsByClassName("carousel__item2");
+// const totalslides2 = slides2.length;
 
-document.getElementById("carousel__btn--next2")
-.addEventListener("click", function(){
-    moveNextSlide2();
-});
-document.getElementById("carousel__btn--prev2")
-.addEventListener("click", function(){
-    movePrevSlide2();
-});
-function updateSlidePositions(){
-    for(let slide of slides2){
-        slide.classList.remove("carousel__item--visible2");
-        slide.classList.add("carousel__item--hidden2");
-    }
-    slides2[slide2].classList.add("carousel__item--visible2");
-}
-function moveNextSlide2(){
+// document.getElementById("carousel__btn--next2")
+// .addEventListener("click", function(){
+//     moveNextSlide2();
+// });
+// document.getElementById("carousel__btn--prev2")
+// .addEventListener("click", function(){
+//     movePrevSlide2();
+// });
+// function updateSlidePositions(){
+//     for(let slide of slides2){
+//         slide.classList.remove("carousel__item--visible2");
+//         slide.classList.add("carousel__item--hidden2");
+//     }
+//     slides2[slide2].classList.add("carousel__item--visible2");
+// }
+// function moveNextSlide2(){
     
-    if (slide2 === totalslides2 -1){
-        slide2 = 0;
-    } else {
-        slide2++;
-    }
-    updateSlidePositions();
-}
-function movePrevSlide2(){
+//     if (slide2 === totalslides2 -1){
+//         slide2 = 0;
+//     } else {
+//         slide2++;
+//     }
+//     updateSlidePositions();
+// }
+// function movePrevSlide2(){
    
-    if (slide2 === 0){
-    // totalslides2 -1, if you wan't infinite carousel effect to both ways
-        slide2 = 1;  
-    } else {
-        slide2--;
-    }
-    updateSlidePositions();
-}
+//     if (slide2 === 0){
+//     // totalslides2 -1, if you wan't infinite carousel effect to both ways
+//         slide2 = 1;  
+//     } else {
+//         slide2--;
+//     }
+//     updateSlidePositions();
+// }
 //second hide and show
-const element3 = document.querySelectorAll('.element3');
-element3.forEach(function(el3, key3){                
+const element77 = document.querySelectorAll('.element3');
+element77.forEach(function(el3, key3){                
     el3.addEventListener('click', function () {
         el3.classList.add("active3");
             
-        element3.forEach(function(ell3, index3){
+        element77.forEach(function(ell3, index3){
             if(key3 !== index3) {
                 ell3.classList.remove('active3');
             }
